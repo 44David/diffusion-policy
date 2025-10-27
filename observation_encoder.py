3b6a66d4b0403aec48911114c85cba75872f1a65
44David <david.soane@outlook.com>
2025-10-27 17:01:55 -0600

    wrote state based observation encoder

---
+++ observation_encoder.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+
+class MLPEncoder(nn.Module):
+    '''
+    Simple MLP based encoder, used when dealing with state based observations
+    '''
+    
+    def __init__(self, state_dim, observation_embed_dim ):
+        super().__init__()
+        self.fc1 = nn.Linear(state_dim, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, observation_embed_dim)
+        self.relu = nn.ReLU
+        
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        
+        return x
+        
